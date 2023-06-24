2f242a3191f9b68f250b53bebcaffa08ab941db5
Filip Šipoš <filipsipo@gmail.com>
2023-06-24 09:05:04 +0200

    Update DoctorMap.vue

--- vue_frontend/src/views/DoctorMap.vue
+++ vue_frontend/src/views/DoctorMap.vue
@@ -6,8 +6,47 @@ import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 </script>
 <script>
-let myLocation;
-let targetLocation;
+//Get this from GPS
+let start = [-123.069003, 45.395273];
+
+//Get this from server
+let targetLocation = [-123.169003, 45.495273];
+
+async function getRoute(end) {
+  const query = await fetch(
+    `https://api.mapbox.com/directions/v5/mapbox/cycling/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
+    { method: "GET" }
+  );
+  const json = await query.json();
+  const data = json.routes[0];
+  const route = data.geometry.coordinates;
+  const geojson = {
+    type: "Feature",
+    properties: {},
+    geometry: {
+      type: "LineString",
+      coordinates: route,
+    },
+  };
+
+  map.addLayer({
+    id: "route",
+    type: "line",
+    source: {
+      type: "geojson",
+      data: geojson,
+    },
+    layout: {
+      "line-join": "round",
+      "line-cap": "round",
+    },
+    paint: {
+      "line-color": "#3887be",
+      "line-width": 5,
+      "line-opacity": 0.75,
+    },
+  });
+}
 
 export default {
   name: "DoctorMap",
@@ -18,12 +57,54 @@ export default {
     const map = new mapboxgl.Map({
       container: "map", // container ID
       style: "mapbox://styles/mapbox/light-v10", // style URL
-      center: myLocation, // starting position [lng, lat]
+      center: start, // starting position [lng, lat]
       zoom: 17, // starting zoom
     });
 
     map.scrollZoom.disable();
+
+    //Call the directions API
+    map.on("load", () => {
+      // make an initial directions request that
+      // starts and ends at the same location
+      getRoute(start);
+
+      // Add starting point to the map
+      map.addLayer({
+        id: "point",
+        type: "circle",
+        source: {
+          type: "geojson",
+          data: {
+            type: "FeatureCollection",
+            features: [
+              {
+                type: "Feature",
+                properties: {},
+                geometry: {
+                  type: "Point",
+                  coordinates: start,
+                },
+              },
+            ],
+          },
+        },
+        paint: {
+          "circle-radius": 10,
+          "circle-color": "#3887be",
+        },
+      });
+      // this is where the code from the next step will go
+    });
+    // reset if already exists, make a new request if it doesn't
+
+    //Make the initial call
   },
 };
 </script>
-<style lang="scss"></style>
+<style lang="scss">
+#map {
+  width: 100%;
+  height: calc(100% + 2rem);
+}
+</style>
