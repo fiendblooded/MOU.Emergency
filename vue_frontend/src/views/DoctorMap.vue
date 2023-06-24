a33b272ae6986d67c07506d481239c970eb174ee
Filip Šipoš <filipsipo@gmail.com>
2023-06-24 12:57:30 +0200

    Update DoctorMap.vue

--- vue_frontend/src/views/DoctorMap.vue
+++ vue_frontend/src/views/DoctorMap.vue
@@ -1,5 +1,47 @@
 <template>
   <div id="map"></div>
+  <div class="overlay">
+    <v-card
+      class="rounded-xl px-0 py-4 flex-column justify-center align-center contentcard"
+    >
+      <button class="up" @click="hore">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M4.5 15.75l7.5-7.5 7.5 7.5"
+          />
+        </svg>
+      </button>
+      <v-card-title class="text-center mb-3">Lekársky záznam</v-card-title>
+      <v-card-text class="px-3">
+        <v-list>
+          <v-list-item v-for="item in medicalInfo" class="flex-list px-2">
+            <span class="title">{{ item.title }}</span
+            ><span class="val">{{ item.value }}</span>
+          </v-list-item>
+        </v-list>
+        <v-expansion-panels class="ma-0 pa-0 nosh" variant="accordion">
+          <v-expansion-panel v-for="panel in expansionPanels" class="nosh">
+            <v-expansion-panel-title class="px-2 nosh title">{{
+              panel.title
+            }}</v-expansion-panel-title>
+            <v-expansion-panel-text class="text-center">
+              <!-- FOR LOOP -->
+              <span class="text-bold">{{ panel.content.join(", ") }}</span>
+            </v-expansion-panel-text>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </v-card-text>
+    </v-card>
+  </div>
 </template>
 <script setup>
 import mapboxgl from "mapbox-gl";
@@ -12,6 +54,7 @@ import { emergency, socket } from "@/socket";
 export default {
   name: "DoctorMap",
   data: () => ({
+<<<<<<< Updated upstream
     interval: null,
     map: null,
     start: false,
@@ -46,6 +89,29 @@ export default {
           },
         ],
       };
+=======
+    bottomsheet: true,
+    medicalInfo: [
+      { title: "Pohlavie", value: "Muž" },
+      { title: "Pohlavie", value: "Muž" },
+      { title: "Pohlavie", value: "Muž" },
+    ],
+    expansionPanels: [
+      { title: "Alergény", content: ["Penicilín", "Prachy"] },
+      { title: "lol", content: "lorem" },
+      { title: "lol", content: "lorem" },
+      { title: "lol", content: [] },
+    ],
+  }),
+  methods: {
+    hore() {
+      console.log("hoer");
+      document.querySelector(".overlay").classList.toggle("idemhore");
+    },
+  },
+  mounted() {
+    console.log("Emergency:", JSON.parse(JSON.stringify(emergency)));
+>>>>>>> Stashed changes
 
       if(this.map.getLayer('point')) {
         this.map.getSource('point').setData(point);
@@ -208,6 +274,59 @@ export default {
 #map {
   width: 100%;
   height: calc(100% + 2rem);
+}
+
+.idemhore {
+  transform: translate(-50%, 10%) !important;
+}
+
+.contentcard {
+  height: 70vh;
+}
+
+.text-bold {
+  font-weight: 500;
+  text-align: center;
+}
+
+.v-list-item__content {
+  display: flex !important;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between !important;
+}
+
+.title {
+  color: rgb(70, 70, 70);
+  font-weight: 500;
+}
+
+.val {
+  font-weight: 600;
+}
+
+.nosh {
+  box-shadow: none !important;
+}
+
+.up {
+  width: 1.6rem;
+  display: block !important;
+  width: 100%;
+  height: 1.6rem;
+  svg {
+    width: 1.6rem;
+    height: 1.6rem;
+  }
+}
+.overlay {
+  position: fixed;
+  left: 50%;
+  transform: translate(-50%, 90%);
+  bottom: 5%;
+  width: calc(100% - 1rem);
+  transition: all 0.4s ease-in;
+  z-index: 1000000000;
 }
 
 .marker {
