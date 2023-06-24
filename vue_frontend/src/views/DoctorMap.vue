0c3a109279b123fe7bfe830adfd27b954b626cce
Filip Šipoš <filipsipo@gmail.com>
2023-06-24 10:29:08 +0200

    Update DoctorMap.vue

--- vue_frontend/src/views/DoctorMap.vue
+++ vue_frontend/src/views/DoctorMap.vue
@@ -7,19 +7,23 @@ import "mapbox-gl/dist/mapbox-gl.css";
 </script>
 <script>
 //Get this from GPS
-let start = [-123.069003, 45.395273];
+let start = [-123.160603, 45.396573];
 
 //Get this from server
 let targetLocation = [-123.169003, 45.495273];
 
-async function getRoute(end, mapObj) {
+async function getRoute(end) {
   const query = await fetch(
     `https://api.mapbox.com/directions/v5/mapbox/cycling/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
     { method: "GET" }
   );
+
   const json = await query.json();
   const data = json.routes[0];
+
   const route = data.geometry.coordinates;
+  console.log("data", data);
+
   const geojson = {
     type: "Feature",
     properties: {},
@@ -28,28 +32,64 @@ async function getRoute(end, mapObj) {
       coordinates: route,
     },
   };
+  if (mapD.getSource("route")) {
+    console.log("Route already exists on the map");
+    mapD.getSource("route").setData(geojson);
+  } else {
+    console.log("creating new route");
+    console.log(mapD);
+    mapD.addLayer({
+      id: "route",
+      type: "line",
+      source: {
+        type: "geojson",
+        data: route,
+      },
+      layout: {
+        "line-join": "round",
+        "line-cap": "round",
+      },
+      paint: {
+        "line-color": "#3887be",
+        "line-width": 5,
+        "line-opacity": 0.75,
+      },
+    });
+  }
 
-  mapObj.addLayer({
-    id: "route",
-    type: "line",
-    source: {
-      type: "geojson",
-      data: geojson,
-    },
-    layout: {
-      "line-join": "round",
-      "line-cap": "round",
-    },
-    paint: {
-      "line-color": "#3887be",
-      "line-width": 5,
-      "line-opacity": 0.75,
-    },
-  });
+  // if (mapObj.getSource("route")) {
+  //   console.log("Route already exists on the map");
+  //   mapObj.getSource("route").setData(geojson);
+  // } else {
+  //   console.log("creating new route");
+  //   console.log(mapObj);
+  //   mapObj.addLayer({
+  //     id: "route",
+  //     type: "line",
+  //     source: {
+  //       type: "geojson",
+  //       data: geojson,
+  //     },
+  //     layout: {
+  //       "line-join": "round",
+  //       "line-cap": "round",
+  //     },
+  //     paint: {
+  //       "line-color": "#3887be",
+  //       "line-width": 5,
+  //       "line-opacity": 0.75,
+  //     },
+  //   });
+  // }
 }
 
 export default {
   name: "DoctorMap",
+  data() {
+    return {
+      mapD: {},
+    };
+  },
   mounted() {
     mapboxgl.accessToken =
       "pk.eyJ1IjoiZmlsaXBzaXBvcyIsImEiOiJjbGo4b2VxdXMxN3VzM2VxenlqbDhyZG14In0.tEoQDyIZe6DeE02GszDilw";
@@ -60,22 +100,71 @@ export default {
       center: start, // starting position [lng, lat]
       zoom: 17, // starting zoom
     });
+    this.mapD = map;
+    console.log(this.mapD);
 
-    map.scrollZoom.disable();
+    this.mapD.scrollZoom.disable();
 
     //Call the directions API
-    map.on("load", () => {
+    this.mapD.on("load", () => {
       console.log("sme tu");
       // make an initial directions request that
       // starts and ends at the same location
-      getRoute(start, map);
+      getRoute(start);
+      map.addLayer({
+        id: "point",
+        type: "circle",
+        source: {
+          type: "geojson",
+          data: {
+            type: "FeatureCollection",
+            features: [
+              {
+                type: "Feature",
+                properties: {},
+                geometry: {
+                  type: "Point",
+                  coordinates: start,
+                },
+              },
+            ],
+          },
+        },
+        paint: {
+          "circle-radius": 10,
+          "circle-color": "#3887be",
+        },
+      });
       // console.log(map.addLayer());
       // Add starting point to the map
-      if (map.getSource("route")) {
-        map.getSource("route").setData(geojson);
+    });
+    // reset if already exists, make a new request if it doesn't
+
+    //Make the initial call
+
+    map.on("click", (event) => {
+      const coords = Object.keys(event.lngLat).map((key) => event.lngLat[key]);
+      let route = getRoute(coords).then((route) => {
+        console.log(route);
+      });
+      const end = {
+        type: "FeatureCollection",
+        features: [
+          {
+            type: "Feature",
+            properties: {},
+            geometry: {
+              type: "Point",
+              coordinates: coords,
+            },
+          },
+        ],
+      };
+      if (map.getLayer("end")) {
+        map.getSource("end").setData(end);
       } else {
         map.addLayer({
-          id: "point",
+          id: "end",
           type: "circle",
           source: {
             type: "geojson",
@@ -87,7 +176,7 @@ export default {
                   properties: {},
                   geometry: {
                     type: "Point",
-                    coordinates: start,
+                    coordinates: coords,
                   },
                 },
               ],
@@ -95,15 +184,13 @@ export default {
           },
           paint: {
             "circle-radius": 10,
-            "circle-color": "#3887be",
+            "circle-color": "#f30",
           },
         });
       }
-    });
-    // reset if already exists, make a new request if it doesn't
 
-    //Make the initial call
-    getRoute([-122.677738, 45.522458], map);
+      // getRoute(coords, map);
+    });
   },
 };
 </script>
